63f394ef26f284cd11dfd7373b3e91553ed9dad2
nhn1030 <shgusska12@gmail.com>
2026-02-12 18:27:03 +0900

    🔖 Set version to 0.0.1"

--- fastuator/__init__.py
+++ fastuator/__init__.py
@@ -3,7 +3,7 @@ fastuator - Production-ready monitoring for FastAPI
 K8s probes, Prometheus metrics, health checks in 1 line.
 """
 
-__version__ = "0.1.0"
+__version__ = "0.0.1"
 
 from .core import Fastuator
 from .checks import cpu_health, disk_health, memory_health
